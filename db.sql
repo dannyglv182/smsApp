64150b7d7e20e8463f5ebea0ba8659bdee50fd6a
Danny <dannyglv182@gmail.com>
2021-12-13 17:21:15 -0800

    Made initial database and tables for postgres.

--- db.sql
+++ db.sql
@@ -1,31 +1,24 @@
+DROP DATABASE IF EXISTS testdb;
 
-DROP TABLE IF EXISTS form, question;
+CREATE DATABASE testdb
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
 
-CREATE TABLE form (
-  id INT NOT NULL AUTO_INCREMENT,
-  name varchar(30) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE message (
+  message_id INTEGER PRIMARY KEY,
+  body TEXT
 );
 
-CREATE TABLE question (
-  id INT NOT NULL AUTO_INCREMENT,
-  form_id INT,
-  FOREIGN KEY (form_id)
-    REFERENCES form(id)
-    ON DELETE CASCADE,
-  text varchar (256),
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE questionSent (
-  id INT NOT NULL AUTO_INCREMENT,
-  form_id INT,
-  question_id INT,
-  FOREIGN KEY (form_id)
-    REFERENCES form(id)
-    ON DELETE CASCADE,
-  FOREIGN KEY (question_id)
-    REFERENCES question(id)
-    ON DELETE CASCADE,
-  PRIMARY KEY (id)
-);
+CREATE TABLE receipt (
+    receipt_id INTEGER PRIMARY KEY,
+    message_id INTEGER,
+    Receiver_Number TEXT,
+    CONSTRAINT fk_message
+      FOREIGN KEY(message_id)
+        REFERENCES message(message_id)
+  );
