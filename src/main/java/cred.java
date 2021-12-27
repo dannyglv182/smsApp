02302b07d6fde4474b89b4f7d0cc165cc71d42c1
Danny <dannyglv182@gmail.com>
2021-12-26 20:54:21 -0800

    Added file for postgres and twilio info.

---
+++ src/main/java/cred.java
@@ -0,0 +1,7 @@
+public class cred {
+    static String url = "";
+    static String password = "";
+    static String twilUsername = "";
+    static String twilPassword = "";
+    static String twilPhoneNUmber = "";
+}
