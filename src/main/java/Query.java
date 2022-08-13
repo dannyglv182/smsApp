0fcbef452ffa94620e287a0490552a686cddb222
Danny <dannyglv182@gmail.com>
2022-08-13 13:21:22 -0700

    Fixed comments in file that creates queries.

--- src/main/java/Query.java
+++ src/main/java/Query.java
@@ -4,20 +4,27 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
 
-/**
-* Returns a PostgreSQL INSERT operation of type string
- * This is used to add a copy of the sms that was sent to the database.
- * The string returned here is used by JDBC to execute the query against the database.
- * @return insertOp
- */
 public class Query {
 
+    /**
+     * Returns a PostgreSQL INSERT operation in the form of a string.
+     * The string returned here is used by JDBC to insert a message into the database.
+     * @return insertOp
+     */
     public static String insertMessage(String messageBody){
         // fields Zero-index = recipient's number, 1th-index = message body
         String insertOp = "INSERT INTO message (body) VALUES ('" + messageBody + "')";
         return insertOp;
     }
 
+    /**
+     * Returns a postGresql INSERT operation in the form of a string.
+     * The string returned here is used by JDBC to insert a receipt containing
+     * the message id and receiver number into the database.
+     * @param messageID
+     * @param reveiverNumber
+     * @return insertOp
+     */
     public static String insertReceipt(int messageID, String reveiverNumber){
         String messageId = String.valueOf(messageID);
         String insertOp = "INSERT INTO receipt (message_id, receiver_number) " +
